2b836333d464181e80714901f6cf358ebe141337
DocRyze <dongkang_magic@aliyun.com>
2023-05-10 17:09:04 +0800

    init

---
+++ include/player.h
@@ -0,0 +1,29 @@
+#ifndef PLAY_H_
+#define PLAY_H_
+#include "raylib.h"
+class Player
+{
+  public:
+    Player();
+    Player(Player &&) = default;
+    Player(const Player &) = default;
+    Player &operator=(Player &&) = default;
+    Player &operator=(const Player &) = default;
+    ~Player();
+
+  protected:
+    // 初始化
+    void OnReady();
+    // 处理
+    void Process(float lastFrameTime);
+    // 物理处理
+    void PhysicsProcess(float lastFrameTime);
+
+  private:
+    // 贴图
+    Texture2D texture;
+    // 轮廓
+    Vector2 outline[];
+};
+
+#endif // !PLAY_H_
---
+++ src/player.cpp
@@ -0,0 +1,21 @@
+#include "player.h"
+
+Player::Player()
+{
+}
+
+Player::~Player()
+{
+}
+
+void Player::OnReady()
+{
+}
+
+void Player::Process(float lastFrameTime)
+{
+}
+
+void Player::PhysicsProcess(float lastFrameTime)
+{
+}
